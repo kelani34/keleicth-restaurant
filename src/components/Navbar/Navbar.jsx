d30456b4f1f865174b9c647a1807cca4c5351d73
kelani34 <kelanitaiwo34@gmail.com>
2022-12-22 01:49:56 +0100

    add: navbar content

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,8 +7,27 @@ import "./Navbar.css";
 import "./Navbar.css";
 
 const Navbar = () => (
-  <nav className="app_navbar">
-    <div>Nav</div>
+  <nav className="app__navbar">
+    <div className="app__navbar-logo">
+      <img src={images.gericht} alt="logo" />
+    </div>
+    <ul className="app__navbar-links">
+      <li className="p__opensans">
+        <a href="#home">Home</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#about">About</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#menu">Menu</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#awards">Awards</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#contact">Contact</a>
+      </li>
+    </ul>
   </nav>
 );
 
