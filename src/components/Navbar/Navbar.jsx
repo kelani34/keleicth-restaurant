37626f5f0aa29ed73bfaf05cdd08ee045f36040f
kelani34 <kelanitaiwo34@gmail.com>
2022-12-22 01:42:11 +0100

    fix: navbar setup

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+import images from "../../constants/images";
+import "./Navbar.css";
 
-import './Navbar.css';
+import "./Navbar.css";
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav className="app_navbar">
+    <div>Nav</div>
+  </nav>
 );
 
 export default Navbar;
