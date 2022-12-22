4baa2cd9952da0c6cfc95719e8a1724dbcdcbe39
kelani34 <kelanitaiwo34@gmail.com>
2022-12-22 16:23:04 +0100

    update: styling for navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -40,7 +40,11 @@ const Navbar = () => (
     <div className="app__navbar-smallscreen">
       <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}} />
       <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
-        <MdOutlineRestaurantMenu fontSize={27} onClick={() => {}} />
+        <MdOutlineRestaurantMenu
+          fontSize={27}
+          className="overlay__close"
+          onClick={() => {}}
+        />
         <ul className="app__navbar-smallscreen-links">
           <li className="p__opensans">
             <a href="#home">Home</a>
