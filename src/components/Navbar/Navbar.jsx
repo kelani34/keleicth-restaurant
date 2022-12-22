492804e7bece8515f76c209658886331872e4af0
kelani34 <kelanitaiwo34@gmail.com>
2022-12-22 13:42:17 +0100

    style: responsive overlay

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -38,7 +38,27 @@ const Navbar = () => (
       </a>
     </div>
     <div className="app__navbar-smallscreen">
-      <GiHamburgerMenu />
+      <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}} />
+      <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+        <MdOutlineRestaurantMenu fontSize={27} onClick={() => {}} />
+        <ul className="app__navbar-smallscreen-links">
+          <li className="p__opensans">
+            <a href="#home">Home</a>
+          </li>
+          <li className="p__opensans">
+            <a href="#about">About</a>
+          </li>
+          <li className="p__opensans">
+            <a href="#menu">Menu</a>
+          </li>
+          <li className="p__opensans">
+            <a href="#awards">Awards</a>
+          </li>
+          <li className="p__opensans">
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
 );
