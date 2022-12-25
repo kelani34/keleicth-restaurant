1fe66161feef113dd53f51e98f2dfb522342bc8b
kelani34 <kelanitaiwo34@gmail.com>
2022-12-25 19:34:35 +0100

    feat: finishup header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import './Header.css';
+import "./Header.css";
+import { images } from "../../constants";
 
 const Header = () => (
-  <div>
-    Header
+  <div id="home" className="app__header app__wrapper section__padding">
+    <div className="app__wrapper_info">
+    </div>
+    <div className="app__wrapper_img"></div>
   </div>
 );
 
